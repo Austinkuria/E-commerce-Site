e9025704e9cd3420ee46f20109b133c49c121764
Austinkuria <kuriaaustin125@gmail.com>
2024-08-07 18:23:35 +0300

    Set up Django project with URL configuration, form, and product model

--- ecommerce/myapp/views.py
+++ ecommerce/myapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request,'index.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
