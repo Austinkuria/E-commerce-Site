739ce36fe0c67a06641de2610c0e155df7830fef
Austinkuria <kuriaaustin125@gmail.com>
2024-08-11 23:02:21 +0300

    register products model ,remove old venv and install new new venv,update settings.py

--- ecommerce/myapp/admin.py
+++ ecommerce/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Products
 
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display =('name','price','description','image')
+    search_fields =('name','description')
+
+admin.site.register(Products,ProductsAdmin)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Teresia\Documents\GitHub\E-commerce-Site\venv
+set VIRTUAL_ENV=C:\Users\AustinM\Documents\GitHub\E-commerce-Site\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
